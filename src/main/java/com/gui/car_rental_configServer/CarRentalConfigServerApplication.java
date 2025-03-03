fa0d5fcae717241b00b5b2b81c0beead1375e7c7
Guilherme Vale <guilherme.vale98@gmail.com>
2025-03-03 18:20:54 -0300

    start config server

---
+++ src/main/java/com/gui/car_rental_configServer/CarRentalConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.gui.car_rental_configServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gui/car_rental_configServer/CarRentalConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gui.car_rental_configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
